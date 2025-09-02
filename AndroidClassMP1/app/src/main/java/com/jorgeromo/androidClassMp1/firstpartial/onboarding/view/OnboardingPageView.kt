db99ea954622d40ad953ae88b752c5f21191d1e6
Jose Holguin <13375@ulsachihuahua.edu>
2025-09-02 16:56:10 -0600

    Se hicieron cambios esteticos, de color y en el onboarding

--- AndroidClassMP1/app/src/main/java/com/jorgeromo/androidClassMp1/firstpartial/onboarding/view/OnboardingPageView.kt
+++ AndroidClassMP1/app/src/main/java/com/jorgeromo/androidClassMp1/firstpartial/onboarding/view/OnboardingPageView.kt
@@ -11,9 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jorgeromo.androidClassMp1.firstpartial.onboarding.model.OnboardingPageModel
 
 /**
@@ -30,12 +33,16 @@ fun OnboardingPageView(pageModel: OnboardingPageModel) {
             contentDescription = pageModel.title,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(280.dp)
+                .height(280.dp),
+            contentScale = ContentScale.Crop
         )
         Spacer(Modifier.height(24.dp))
         Text(
             text = pageModel.title,
-            style = MaterialTheme.typography.headlineSmall,
+            style = MaterialTheme.typography.headlineSmall.copy(
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp // aquí defines el tamaño que quieras
+            ),
             textAlign = TextAlign.Center
         )
         Spacer(Modifier.height(12.dp))
